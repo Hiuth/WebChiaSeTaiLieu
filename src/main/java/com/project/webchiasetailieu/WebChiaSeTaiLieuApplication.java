df1b8790eb68052459a124a262baad97be3d1aca
Hiuth <Hiuth2004@gmail.com>
2024-09-29 09:46:41 +0700

    Initial commit

---
+++ src/main/java/com/project/webchiasetailieu/WebChiaSeTaiLieuApplication.java
@@ -0,0 +1,13 @@
+package com.project.webchiasetailieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChiaSeTaiLieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChiaSeTaiLieuApplication.class, args);
+	}
+
+}
